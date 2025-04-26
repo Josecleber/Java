ea5d006907e89d0edab0ef3267f8f75b1a205b8d
José Cleber <josecleber0910@gmail.com>
2025-04-26 14:03:42 -0300

    Create Emprestimo.java

---
+++ Projeto/Biblioteca/emprestimo/Emprestimo.java
@@ -0,0 +1,50 @@
+package emprestimo;
+
+import livro.Livro;
+import java.time.LocalDate;
+
+public class Emprestimo {
+    private int id;
+    private int usuarioId;
+    private int livroId;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataDevolucao;
+
+    public Emprestimo(int id, int usuarioId, int livroId, LocalDate dataEmprestimo, LocalDate dataDevolucao) {
+        this.id = id;
+        this.usuarioId = usuarioId;
+        this.livroId = livroId;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+    public int getLivroId() {
+        return livroId;
+    }
+    public void setLivroId(int livroId) {
+        this.livroId = livroId;
+    }
+    public LocalDate getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+    public void setDataEmprestimo(LocalDate dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+    public void setDataDevolucao(LocalDate dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+}
