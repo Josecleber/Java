6099e5e2f82f18a1c4040c9da4ea4513a0f5f8dc
José Cleber <josecleber0910@gmail.com>
2025-04-26 15:07:24 -0300

    Create App.java

---
+++ Exercício Banco_Loja Simples/App.java
@@ -0,0 +1,12 @@
+package banco_loja;
+
+public class App {
+    public static void main(String[] args) {
+        Produto p1 = new Produto("Arroz");
+        Produto p2 = new Produto("Feijão");
+        Produto p3 = new Produto("Bolacha");
+        Produto p4 = new Produto("PS5 Pro");
+        Produto p5 = new Produto("Iphone 16");
+        Produto p6 = new Produto();
+    }
+}
