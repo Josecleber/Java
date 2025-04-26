9e4ff21e9b2f0cf7e42bbb42e7b6d3949312bd6c
José Cleber <josecleber0910@gmail.com>
2025-04-26 14:57:11 -0300

    Create Funcionario.java

---
+++ Exercício Sistema de Funcionários e Gerentes/empresa/Funcionario.java
@@ -0,0 +1,46 @@
+package empresa;
+
+public class Funcionario {
+    private int id;
+    private String nome;
+    private double salario;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(int id, String nome, double salario) {
+        this.id = id;
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void mostraDados() {
+        System.out.println("ID: " + id);
+        System.out.println("NOME: " + nome);
+        System.out.println("SALÁRIO: " + salario);
+    }
+}
