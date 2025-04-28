f66c69291612984059b0ab35e47423c97d4c4a4d
José Cleber <josecleber0910@gmail.com>
2025-04-28 14:40:29 -0300

    Create AppCarro.java

---
+++ Exercício carro/AppCarro.java
@@ -0,0 +1,9 @@
+package carro_simples;
+
+public class AppCarro {
+    public static void main(String[] args) {
+        Carro carro = new Carro("Toyota", 2020);
+
+        carro.exibirInformacoes();
+    }
+}
