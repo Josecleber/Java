380f8978d6d6f4123974a51649e02b702e8e65b2
José Cleber <josecleber0910@gmail.com>
2025-04-28 14:54:00 -0300

    Create Quadrado.java

---
+++ Exercício Calcular Área de um Quadrado/Quadrado.java
@@ -0,0 +1,13 @@
+package quadrado_area;
+
+public class Quadrado {
+    private double lado;
+
+    public Quadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public double calcularArea() {
+        return lado * lado;
+    }
+}
